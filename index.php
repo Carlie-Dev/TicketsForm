f2a3a3d7e4e9f6483dbb12c4db86cc81c9480bad
Carlie-Dev <ellepheonixgames@gmail.com>
2025-08-29 22:26:25 -0500

    variables

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<?php 
+    if ($_GET){
+        $customername = $_GET['customername'];
+        $tickets = (int) $_GET['numberoftickets'];
+        $coupon = $_GET['coupon'];
+    }
+    echo $customername;
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,17 +14,25 @@
     <title>Ticket Form</title>
 </head>
 <body>
-    <form action="get">
-        <label for="name">Your Name:</label>
-        <input type="text" name="customername" id="name">
-        <label for="tickets">How Many tickets do you want?</label>
-        <input type="text" name="numberoftickets" id="tickets">
-        <p>Do you have a coupon?</p>
-        <input type="radio" name="coupon" id="yes">
-        <label for="yes">Yes</label>
-        <input type="radio" name="coupon" id="no">
-        <label for="no">No</label>
-    </form>
+    <div>
+        <form action="get">
+                <label for="customername">Your Name:</label>
+                <input type="text" name="customer" id="customername">
+                <label for="tickets">How Many tickets do you want?</label>
+                <input type="text" name="numberoftickets" id="tickets">
+                <p>Do you have a coupon?</p>
+                <input type="radio" name="coupon" id="yes">
+                <label for="yes">Yes</label>
+                <input type="radio" name="coupon" id="no">
+                <label for="no">No</label>
+                <input type="submit" value="Submit">
+            </form>
+    </div>
+<!--logic-->
+    <div>
+        <?php 
+        ?>
+    </div>
     
 </body>
 </html>
