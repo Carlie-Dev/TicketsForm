fbbc45c2d13cf5bbe2f2adbdd5186028cd09684a
Carlie-Dev <ellepheonixgames@gmail.com>
2025-08-29 22:10:58 -0500

    basic form structure

--- index.php
+++ index.php
@@ -6,6 +6,17 @@
     <title>Ticket Form</title>
 </head>
 <body>
+    <form action="get">
+        <label for="name">Your Name:</label>
+        <input type="text" name="customername" id="name">
+        <label for="tickets">How Many tickets do you want?</label>
+        <input type="text" name="numberoftickets" id="tickets">
+        <p>Do you have a coupon?</p>
+        <input type="radio" name="coupon" id="yes">
+        <label for="yes">Yes</label>
+        <input type="radio" name="coupon" id="no">
+        <label for="no">No</label>
+    </form>
     
 </body>
 </html>
